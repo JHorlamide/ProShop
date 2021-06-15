87cf8f48c880bb3e668b6be5e0ed364df40848f5
Horlamide <olamide.jubril@taxtech.com.ng>
2021-06-16 00:31:38 +0100

    Connecting front with backend

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import './bootstrap.min.css'
+import './bootstrap.min.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+/* Redux Implementation */
+import { Provider } from 'react-redux';
+import { applyMiddleware, createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import thunk from 'redux-thunk';
+import reducers from './reducers';
+
+/* Redux Initialization */
+const initialState = {};
+const middleware = [thunk];
+
+/* Create store */
+const store = createStore(
+  initialState,
+  reducers,
+  composeWithDevTools(applyMiddleware(...middleware))
+);
+
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
