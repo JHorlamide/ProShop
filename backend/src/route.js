cdf2f6bd2b5930c1d7ad0eb3b15589d5549dce91
Horlamide <olamide.jubril@taxtech.com.ng>
2021-09-18 12:36:29 +0100

    [Backend] preping backend for deployment

--- backend/src/route.js
+++ backend/src/route.js
@@ -35,12 +35,11 @@ const route = (app) => {
 
 	/* App deployment config */
 	if (process.env.NODE_ENV === 'production') {
-		app.use(express.static(path.join(__dirname, '../../frontend/build')));
+		app.use(express.static(path.join(__dirname, '../frontend/build')));
 
 		app.use('*', (req, res) => {
 			return res.sendFile(
-				// path.resolve(__dirname, '/frontend', 'build', 'index.html')
-				path.resolve(__dirname, '../../frontend/build/index.html')
+				path.resolve(__dirname, '../frontend', 'build', 'index.html')
 			);
 		});
 	} else {
