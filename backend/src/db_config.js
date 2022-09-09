642d83a2d749c7b6ab3cf89b00fa50a2dd869214
JHorlamide <olamidejubril68@gmail.com>
2022-09-09 19:48:25 +0100

    Added GCP yaml file

--- backend/src/db_config.js
+++ backend/src/db_config.js
@@ -1,18 +1,23 @@
-import mongoose from 'mongoose';
-// process.env.MONGO_URI
+import mongoose from "mongoose";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const DB_URL =
+  process.env.NODE_ENV === "production"
+    ? process.env.MONGO_URI
+    : process.env.DB_URL_LOCAL;
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(' mongodb://localhost:27017/proshop', {
+    const conn = await mongoose.connect(DB_URL, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
       useFindAndModify: false,
     });
 
-    console.log(
-      `Mongodb connected... ${conn.connection.host}`
-    );
+    console.log(`Mongodb connected... ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
